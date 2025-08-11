fe69b79afe666a9ed6f26b4fd8c7f4b433695de3
QianhengZhang <hazeland3427@gmail.com>
2025-08-11 15:46:36 -0500

    edit name

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: '/p7/',
+    base: '/HongKongMusicAtlas/',
     build: {
         outDir: 'docs'
     }
